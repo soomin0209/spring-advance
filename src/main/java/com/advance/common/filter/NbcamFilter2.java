8ae5550899d8f003c634b9f9928f4bd231cc295f
soomin0209 <soomin02094@gmail.com>
2026-01-22 21:12:03 +0900

    1-1 Filter 실습

--- src/main/java/com/advance/common/filter/NbcamFilter2.java
+++ src/main/java/com/advance/common/filter/NbcamFilter2.java
@@ -11,19 +11,21 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@Order(1)
-public class NbcamFilter extends OncePerRequestFilter {
+@Order(2)
+public class NbcamFilter2 extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
         // 요청이 들어올 때 실행되는 부분
-        System.out.println("NbcamFilter로 들어감");
+        System.out.println("NbcamFilter2로 들어감");
 
         filterChain.doFilter(request, response);
 
         // 요청이 나갈 때 실행되는 부분
-        System.out.println("NbcamFilter로 나감");
+        System.out.println("NbcamFilter2로 나감");
     }
+
+    // 요청이 들어옴 -> NbcamFilter -> NbcamFilter2 -> UserController -> NbcamFilter2 -> NbcamFilter
 }
